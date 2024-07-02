0eb371cc113241d44ad9b4dbafeb6fa548180024
yusukepenguin <37463323+yusukepenguin@users.noreply.github.com>
2024-07-02 16:02:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', async (e) => {
     };
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbzwKuyyQ0CdM4RctP8wKuvWyEH5rT1iCPm5XI6SbKbCUaxmxTMx4v-fxzr08SKKJUct/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbwG1oSsQvTwYPtaOZXMUsTqYZNqxtI5cU3WiUxFQ4zHNJ7uUPgpL2mbA423Nmy_0VIz/exec', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
